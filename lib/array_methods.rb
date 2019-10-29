863c302f0d6f63e6d9f44a43a2cbb6fd6e5c2661
Anna Wijetunga <annawijetunga@gmail.com>
2019-10-29 18:36:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,3 +1,12 @@
+#takes in two parameters: array of integers and a value to find 
+
+#should return the index of the value that was passed in 
+#if the value isn't found, should return nil 
+#loop over the array 
+#compare each value in the array to the value provided
+#this is not the value that needs to be returned
+
+
 def find_element_index(array, value_to_find)
   # Add your solution here
 end
